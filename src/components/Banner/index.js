ca66cd202a89c9b97630b73ba7a5cf76f6fe6df3
Luiz <nandoluizprimeiro@gmail.com>
2025-06-16 18:28:43 -0300

    Criando estrutura básica do projeto poesia em tirinhas

---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img src='/images/BannerPoesia.png' alt='O banner do projeto'/>
+        </header>
+    )
+}
+export default Banner;
