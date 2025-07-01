68a7d51e0e12f33170866a39544342c89c78ae93
Luiz <nandoluizprimeiro@gmail.com>
2025-07-01 15:37:43 -0300

    Criando componente de RoleSection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Banner from './components/Banner'
 import Form from './components/Form';
+import RoleSection from './components/RoleSection';
 
 function App() {
+
+  const roles =[
+    {
+      nome: 'Artista',
+    },
+    {
+      nome: 'Orientador',
+    },
+    {
+      nome: 'Mídia',
+    },
+  ]
+
   return (
     <div className="App">
       <Banner/>
       <Form/>
+      {roles.map(role =><RoleSection nome={role.nome}/>)}
+
+
     </div>
   );
 }
---
+++ src/components/RoleSection/index.js
@@ -0,0 +1,8 @@
+const RoleSection =(props)=>{
+    return(
+        <section className='role-section'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+export default RoleSection;
